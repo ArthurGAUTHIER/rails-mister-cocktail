894d7ff289c2af93b1cc6c93c0391d5fc4f0d148
Arthur GAUTHIER <arthurgauthier.as@gmail.com>
2018-08-17 17:40:44 +0200

    Exercice 24-01 Done

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,14 @@
 Ingredient.create(name: 'lemon')
 Ingredient.create(name: 'ice')
 Ingredient.create(name: 'mint leaves')
+
+require 'json'
+require 'open-uri'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+cocktails_serialized = open(url).read
+cocktails = JSON.parse(cocktails_serialized)
+
+cocktails['drinks'].each do |cocktail|
+  Ingredient.create(name: cocktail['strIngredient1'])
+end
